9f05f283537857a785a30eadd7d65440b3909f88
Akshay Shah <akshay.vjti11@gmail.com>
2019-05-17 10:30:43 -0700

    updated the download snippet

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -4,6 +4,13 @@ from multiprocessing.pool import ThreadPool
 import os
 import requests
 
+import sys,multiprocessing, urllib3, csv
+from PIL import Image
+from io import BytesIO
+from tqdm  import tqdm
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 class Preprocessor:
 	def __init__(self):
 		self
@@ -20,16 +27,62 @@ class Preprocessor:
 						f.write(chunk)
 		return path
 
-	def pool_images(self,all_urls):
+	def download_image(self,key_url):
+		#out_dir = sys.argv[2]
+		(filename, url) = key_url
+		#filename = os.path.join(out_dir, '%s.jpg' % key)
+		#filename = key
+
+		if os.path.exists(filename):
+			print('Image %s already exists. Skipping download.' % filename)
+			return
+
 		try:
-			results = ThreadPool(8).imap_unordered(self.fetch_url, all_urls)
+			global client
+			response = client.request('GET', url)#, timeout=30)
+			image_data = response.data
+		except:
+			print('Warning: Could not download image %s from %s' % (filename, url))
+			return
+
+		try:
+			pil_image = Image.open(BytesIO(image_data))
+		except:
+			print('Warning: Failed to parse image %s %s' % (filename,url))
+			return
+
+		try:
+			pil_image_rgb = pil_image.convert('RGB')
+		except:
+			print('Warning: Failed to convert image %s to RGB' % filename)
+			return
+
+		try:
+			pil_image_rgb.save(filename, format='JPEG', quality=90)
+		except:
+			print('Warning: Failed to save image %s' % filename)
+			return
+
+
+	def pool_images(self,key_url_list):
+		try:
+			results = ThreadPool(8).imap_unordered(self.fetch_url, key_url_list)
 			for path in results:
 				print(path)
 		except:
 			print("URL Missing")
 
 
-	def get_all_urls(self,path,data_type):
+	def download_images(self,key_url_list):
+		pool = multiprocessing.Pool(processes=12)
+
+		with tqdm(total=len(key_url_list)) as bar:
+			for _ in pool.imap_unordered(self.download_image, key_url_list):
+			  bar.update(1)
+
+
+
+	def get_key_url_list(self,path,data_type):
 		if data_type == 'TRAINING':
 			write_path = constants.TRAINING_IMAGES
 		if data_type == 'VALIDATION':
@@ -39,9 +92,9 @@ class Preprocessor:
 
 		with open(path, "r") as read_file:
 			data = json.load(read_file)
-		all_urls = [(os.path.join(write_path,i['id']),i['url']) for i in data['images']]
+		key_url_list = [(os.path.join(write_path,i['id']),i['url']) for i in data['images']]
 
-		return all_urls
+		return key_url_list
 
 
 	def load_data(self,path,data_type):
@@ -53,14 +106,16 @@ class Preprocessor:
 		if data_type == 'TESTING':
 			file_path = os.path.join(constants.TESTING_JSON)
 
-		all_urls = self.get_all_urls(file_path,data_type)
-		self.pool_images(all_urls)
+		key_url_list = self.get_key_url_list(file_path,data_type)
+		self.download_images(key_url_list)
 
 
 		pass
 
 if __name__ == '__main__':
 	preproc = Preprocessor()
+
+	client = urllib3.PoolManager(500)
 
 	if not os.path.exists(constants.TRAINING_IMAGES):
 		os.makedirs(constants.TRAINING_IMAGES)
